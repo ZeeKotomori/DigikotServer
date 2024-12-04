6ffcedabae9c0c09b7e2c660f68080f1e796321f
ZeeKotomori <zikriaf23@gmail.com>
2024-12-04 17:54:28 +0700

    feat: - api/v1/auth - api/v1/ - check database connection - api listen up

---
+++ src/index.js
@@ -0,0 +1,28 @@
+import express from 'express';
+import { PrismaClient } from '@prisma/client';
+import authRoute from './routes/authRoutes.js';
+import angkotRoute from './routes/angkotRoutes.js';
+
+const app = express();
+const prisma = new PrismaClient();
+
+app.use(express.json());
+
+app.use('/api/v1/auth', authRoute);
+app.use('/api/v1/', angkotRoute);
+
+const checkDbConnection = async () => {
+    try {
+        await prisma.$connect();
+        console.log('DB Tersambung');
+    } catch (error) {
+        console.error('Gagal tersambung ke DB:', error);
+    }
+};
+
+checkDbConnection();
+
+const PORT = process.env.PORT || 3001;
+app.listen(PORT, () => {
+    console.log(`Server berjalan di port ${PORT}`);
+});
