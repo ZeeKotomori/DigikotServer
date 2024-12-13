999491e92b4d918b5fa3d9f4d214241cb3fb3435
ZeeKotomori <zikriaf23@gmail.com>
2024-12-13 17:12:39 +0700

    feat : funct driverChooseAngkot

--- src/controller/userController.js
+++ src/controller/userController.js
@@ -37,4 +37,52 @@ class UserController {
             return res.status(500).send("Internal Server Error");
         }
     }
+
+    static async driverChooseAngkot(req, res) {
+        const { id } = req.params;
+        const { idAngkot } = req.body;
+
+        try {
+            const angkot = await prisma.angkot.findUnique({
+                where: { id: idAngkot },
+                select: { supirId: true },
+            });
+    
+            if (!angkot) return res.status(404).send({ error: "Angkot not found" });
+            
+            // jika angkot sudah memiliki supir (supirId) dan supirId bukan lah id yang sedang di gunakan maka return error handler
+            if (angkot.supirId && angkot.supirId !== id) return res.status(400).send({ error: "The public transportation has been used by another driver" });
+
+            const currentAngkot = await prisma.angkot.findFirst({
+                where: { supirId: id },
+            });
+
+            if (currentAngkot) {
+                // Unsign dari angkot lama
+                await prisma.angkot.update({
+                    where: { id: currentAngkot.id },
+                    data: { supirId: null },
+                });
+            }
+
+            const updateAngkot = await prisma.angkot.update({
+                where : {
+                    id : idAngkot
+                },
+                data : {
+                    supirId : id
+                },
+                include : {
+                    supir : true
+                }
+            });
+
+            return res.status(200).send({msg : "Driver Has Been Assigned to Angkot", updateAngkot});
+        } catch (error) {
+            console.log(error);
+            return res.status(500).send({msg : "Internal Server Error"});
+        }
+    }
 }
+
+export default UserController;
