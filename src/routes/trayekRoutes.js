9e63dd5fc2d16f9fbd9f75355447e6d751c0199a
ZeeKotomori <zikriaf23@gmail.com>
2025-01-28 12:57:15 +0700

    feat :
    - add route update
    - add route delete
    - using auntenticateToken
    - using checkRole

--- src/routes/trayekRoutes.js
+++ src/routes/trayekRoutes.js
@@ -1,12 +1,15 @@
 import express from 'express';
 import TrayekController from '../controller/trayekController.js';
+import { authenticateToken } from '../middleware/authMiddleware.js';
+import { checkUserRole } from '../middleware/roleUserCheckMiddleware.js';
 
 const trayekRoutes = express.Router();
 
-// Route untuk mencari trayek tunggal (single trayek)
-trayekRoutes.post('/', TrayekController.createTrayek);
-trayekRoutes.get('/', TrayekController.getAllTrayeks);
-trayekRoutes.get('/findTrayeks', TrayekController.findTrayek);
+trayekRoutes.post('/', authenticateToken,  checkUserRole(['a']), TrayekController.createTrayek);
+trayekRoutes.get('/', authenticateToken, checkUserRole(['a']), TrayekController.getAllTrayeks);
+trayekRoutes.patch('/:id', authenticateToken, checkUserRole(['a']), TrayekController.updateTrayek);
+trayekRoutes.delete('/:id', authenticateToken, checkUserRole(['a']), TrayekController.deleteTrayek);
+trayekRoutes.get('/findTrayeks',authenticateToken, checkUserRole(['a']), TrayekController.findTrayek);
 
 // Route untuk mencari multi trayek
 // trayekRoutes.get('/multi', TrayekController.findMultiTrayek);
