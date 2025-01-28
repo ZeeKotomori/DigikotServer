5034a781d1700124c2dff0c159fc1dcfa4a2ed12
ZeeKotomori <zikriaf23@gmail.com>
2025-01-28 12:16:36 +0700

    feat : newFunction
    - userCreate
    - move removeDriverFromChooseAngkot to supirController
    - move driverChooseAngkot to supirController

--- src/controller/userController.js
+++ src/controller/userController.js
@@ -1,8 +1,38 @@
-import { Prisma, PrismaClient } from "@prisma/client";
+import { PrismaClient } from "@prisma/client";
 
 const prisma = new PrismaClient();
 
 class UserController {
+    static async userCreate(req, res) {
+        const { name, username, email, password} = req.body;
+        if(!name, !username, !email, !password) return res.status(400).send({ msg : "field must been filled" });
+
+        try {
+            const exitingUsername = await prisma.user.findFirst({
+                where: {
+                    username : username
+                }
+            });
+
+            if (exitingUsername) return res.status(400).send({msg : "username alaready exists."});
+            const HashPassword = await bcrypt.hash(password, 10);
+
+            const user = await prisma.user.create({
+                data : {
+                    name,
+                    username,
+                    email,
+                    password : HashPassword
+                }
+            });
+
+            return res.status(201).send({ msg : "User Has Been Created", user : user });
+        } catch (error) {
+            console.log(error);
+            return res.status(500).send("Internal Server Error");
+        }
+    }
+
     static async userUpdate(req, res) {
         const { id } = req.params;
         const { name, username, email, password } = req.body;
@@ -67,82 +97,6 @@ class UserController {
                 }
             });
             return res.status(200).send({ user });
-        } catch (error) {
-            console.log(error);
-            return res.status(500).send("Internal Server Error");
-        }
-    }
-
-    static async removeDriverFromChooseAngkot(req, res) {
-        const { id } = req.params;
-        const { idAngkot } = req.body;
-
-        try {
-            if (!idAngkot) return res.status(400).send({ msg : "Angkot Must Been Choose" });
-            
-            const angkot = await prisma.angkot.findUnique({
-                where: { id: idAngkot },
-            });
-    
-            if (!angkot) return res.status(404).send({ msg: "Angkot not found" });
-
-            if(angkot.supirId !== id) return res.status(400).send({ msg : "This driver is not assigned to the specified angkot" })
-
-            const updatedAngkot = await prisma.angkot.update({
-                where: { id: idAngkot },
-                data: { supirId: null },
-            });
-
-            return res.status(200).send({ msg: "Driver has been successfully removed from the angkot", data: updatedAngkot });
-
-        } catch (error) {
-            console.log(error);
-            return res.status(500).send("Internal Server Error");
-        }
-    }
-
-    static async driverChooseAngkot(req, res) {
-        const { id } = req.params;
-        const { idAngkot } = req.body;
-
-        try {
-            if (!idAngkot) return res.status(400).send({ msg : "Angkot Must Been Choose" });
-            
-            const angkot = await prisma.angkot.findUnique({
-                where: { id: idAngkot },
-                select: { supirId: true },
-            });
-    
-            if (!angkot) return res.status(404).send({ error: "Angkot not found" });
-            
-            // jika angkot sudah memiliki supir (supirId) dan supirId bukan lah id yang sedang di gunakan maka return error handler
-            if (angkot.supirId && angkot.supirId !== id) return res.status(400).send({ error: "The public transportation has been used by another driver" });
-
-            const currentAngkot = await prisma.angkot.findFirst({
-                where: { supirId: id },
-            });
-
-            if (currentAngkot) {
-                // Unsign dari angkot lama
-                await prisma.angkot.update({
-                    where: { id: currentAngkot.id },
-                    data: { supirId: null },
-                });
-            }
-
-            const updateAngkot = await prisma.angkot.update({
-                where : {
-                    id : idAngkot
-                },
-                data : {
-                    supirId : id
-                },
-                include : {
-                    supir : true
-                }
-            });
-
-            return res.status(200).send({msg : "Driver Has Been Assigned to Angkot", updateAngkot});
         } catch (error) {
             console.log(error);
             return res.status(500).send("Internal Server Error");
