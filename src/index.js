66f0c59c322b0cd6a2b4efe32c68e19db66243c7
ZeeKotomori <zikriaf23@gmail.com>
2025-01-28 12:12:04 +0700

    feat :  (server) - add cookie parser - add route trayekRoutes

--- src/index.js
+++ src/index.js
@@ -1,18 +1,23 @@
 import express from 'express';
 import { PrismaClient } from '@prisma/client';
-
+import cookieParser from "cookie-parser";
 import authRoute from './routes/authRoutes.js';
 import angkotRoute from './routes/angkotRoutes.js';
 import userRoutes from './routes/userRoute.js';
+import trayekRoutes from './routes/trayekRoutes.js';
+import supirRoutes from './routes/supirRoute.js';
 
 const app = express();
 const prisma = new PrismaClient();
 
 app.use(express.json());
+app.use(cookieParser());
 
 app.use('/api/v1/auth', authRoute);
 app.use('/api/v1/', angkotRoute);
+app.use('/api/v1/supir', supirRoutes);
 app.use('/api/v1/user', userRoutes);
+app.use('/api/v1/trayek', trayekRoutes);
 
 const checkDbConnection = async () => {
     try {
