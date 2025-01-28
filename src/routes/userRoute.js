95ebbf9bbd3e70e4c9e5fb09e44beb0dd23aa0c8
ZeeKotomori <zikriaf23@gmail.com>
2025-01-28 12:21:07 +0700

    feat : updateUserController
    - add route for userCreate
    - move driverChooseAngkot to supirController
    - move removeDriverFromChooseAngkot to supirController

--- src/routes/userRoute.js
+++ src/routes/userRoute.js
@@ -2,14 +2,14 @@ import express from 'express';
 import UserController from '../controller/userController.js';
 // import  { authenticateToken } from '../middleware/authMiddleware.js';
 import { checkUserRole } from '../middleware/roleUserCheckMiddleware.js'
+import { authenticateToken } from '../middleware/authMiddleware.js';
 
 const userRoutes = express.Router();
 
-userRoutes.post('/pilihAngkot/:id', checkUserRole(['s']), UserController.driverChooseAngkot);
+userRoutes.post('/', authenticateToken, checkUserRole(['a']), UserController.userCreate);
 userRoutes.patch('/update/:id', UserController.userUpdate);
-userRoutes.get('/', UserController.getUsers);
+userRoutes.get('/', authenticateToken, checkUserRole(['a']) ,UserController.getUsers);
 userRoutes.get('/:id', UserController.getUserById);
-userRoutes.patch('/hapusAngkot/:id', checkUserRole(['s']), UserController.removeDriverFromChooseAngkot);
 userRoutes.delete('/:id', UserController.deleteUser);
 
 export default userRoutes;
