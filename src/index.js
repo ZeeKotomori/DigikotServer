140ef2ae817e5eb92c92e7af3a0e0236195511aa
ZeeKotomori <zikriaf23@gmail.com>
2025-02-27 00:40:33 +0700

    feat: add CORS support and error handling middleware; update angkot route path

--- src/index.js
+++ src/index.js
@@ -6,18 +6,28 @@ import angkotRoute from './routes/angkotRoutes.js';
 import userRoutes from './routes/userRoute.js';
 import trayekRoutes from './routes/trayekRoutes.js';
 import supirRoutes from './routes/supirRoute.js';
+import { syncAngkotLocation } from "./service/angkotScheduler.js";
+import cors from 'cors';
 
 const app = express();
 const prisma = new PrismaClient();
 
+app.use(cors(
+    {
+        origin: 'http://localhost:5173',
+        credentials: true,
+    }
+));
 app.use(express.json());
 app.use(cookieParser());
 
 app.use('/api/v1/auth', authRoute);
-app.use('/api/v1/', angkotRoute);
+app.use('/api/v1/angkot', angkotRoute);
 app.use('/api/v1/supir', supirRoutes);
 app.use('/api/v1/user', userRoutes);
 app.use('/api/v1/trayek', trayekRoutes);
+
+syncAngkotLocation();
 
 const checkDbConnection = async () => {
     try {
@@ -29,6 +39,11 @@ const checkDbConnection = async () => {
 };
 
 checkDbConnection();
+
+app.use((err, req, res, next) => {
+    console.error(err.stack);
+    res.status(500).send('Something went wrong!');
+});
 
 const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => {
