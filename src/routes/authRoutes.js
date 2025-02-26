2668aabff66278c2cdd22019a5dc1d928b07ea16
ZeeKotomori <zikriaf23@gmail.com>
2025-02-27 00:39:36 +0700

    feat: add OTP verification and password management routes

--- src/routes/authRoutes.js
+++ src/routes/authRoutes.js
@@ -6,6 +6,10 @@ const authRoute = express.Router();
 
 authRoute.post('/register', AuthController.register);
 authRoute.post('/login', AuthController.login);
+authRoute.post('/verify-otp', AuthController.verifyOtp);
 authRoute.post('/logout', authenticateToken, AuthController.logout);
+authRoute.post('/forgot-password', AuthController.forgotPassword);
+authRoute.post('/reset-password', AuthController.resetPassword);
+authRoute.patch("/update-password", authenticateToken, AuthController.updatePassword);
 
 export default authRoute;
