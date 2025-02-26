50ea266919e19f6f08fe1c1445f4df94f1763cbe
ZeeKotomori <zikriaf23@gmail.com>
2025-02-27 00:39:25 +0700

    feat: add new routes for managing angkot status and user-specific angkot retrieval

--- src/routes/supirRoute.js
+++ src/routes/supirRoute.js
@@ -6,6 +6,9 @@ import { authenticateToken } from '../middleware/authMiddleware.js';
 const supirRoutes = express.Router();
 
 supirRoutes.post('/pilihAngkot/:id', authenticateToken, checkUserRole(['s']), SupirController.driverChooseAngkot);
-supirRoutes.patch('/hapusAngkot/:id', authenticateToken, checkUserRole(['s']), SupirController.removeDriverFromChooseAngkot);
+supirRoutes.patch('/hapusAngkotDariDriver/:id', authenticateToken, checkUserRole(['s']), SupirController.removeDriverFromChooseAngkot);
+supirRoutes.patch('/activeAngkot/:angkotId', authenticateToken, checkUserRole(['s']), SupirController.activeNonActiveAngkot);
+supirRoutes.patch('/updateLocation/:angkotId', authenticateToken, checkUserRole(['s']), SupirController.updateLocation);
+supirRoutes.get('/myAngkot', authenticateToken, checkUserRole(['s']), SupirController.getMyAngkot);
 
 export default supirRoutes;
